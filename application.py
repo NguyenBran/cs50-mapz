feddc35121ee8923f0a740705ab398438880a704
NguyenBran <nguyen.brandon.0526@gmail.com>
2019-08-06 02:59:12 +0000

    Procfile

--- application.py
+++ application.py
@@ -38,7 +38,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///mapz.db")
+db = SQL("postgres://quodhnxqekaccr:42ed38983413e6617acb3c2c55aad545f91166bd886cecf39e646ff9d5f48de0@ec2-107-21-120-104.compute-1.amazonaws.com:5432/d221s270qddtro")
 
 # Make sure API key is set
 if not os.environ.get("API_KEY"):
