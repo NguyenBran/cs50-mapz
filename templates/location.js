5c392f1ad6ee8b3d908118c4afc6fa5deb5989e5
NguyenBran <nguyen.brandon.0526@gmail.com>
2019-08-08 06:03:00 +0000

    js test

--- templates/location.js
+++ templates/location.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
     document.querySelector("form[name='myForm']").addEventListener("submit", function(){
+        alert("here")
         if (navigator.geolocation && document.querySelector('input[name="current"]:checked')){
+            alert("here2")
             navigator.geolocation.getCurrentPosition(function(position){
+                alert("here3")
                 var latitude = position.coords.latitude;
                 var longitude = position.coords.longitude;
                 document.querySelector("input[name='current']").value = latitude + "," + longitude
