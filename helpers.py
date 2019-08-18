4fa3c9a266853f8db1858fef302c32d9ce153ebf
NguyenBran <nguyen.brandon.0526@gmail.com>
2019-08-18 08:09:38 +0000

    redo

--- helpers.py
+++ helpers.py
@@ -196,7 +196,7 @@ def login_required(f):
     """
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if session.get("username") is None:
+        if session.get("user_id") is None:
             return redirect("/login")
         return f(*args, **kwargs)
     return decorated_function
