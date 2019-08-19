eae9e1b82b5369081cef09d27ce72d7f80a46b7f
NguyenBran <nguyen.brandon.0526@gmail.com>
2019-08-19 08:25:06 +0000

    test

--- application.py
+++ application.py
@@ -205,7 +205,7 @@ def login():
 def logout():
     """Log user out"""
 
-    db.execute("SELECT SETVAL('id', lastval()) FROM users")
+    db.execute("SELECT SETVAL('users_id_seq', lastval()) FROM users")
 
     # Forget any username
     session.clear()
